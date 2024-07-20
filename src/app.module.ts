1aa53896a0dd8bf930d5c811cf578362eda26fe0
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-07-20 11:16:08 +0700

    [sessions] use mongo to persist sessions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as cookieParser from 'cookie-parser';
+import { randomBytes } from 'crypto';
+import * as session from 'express-session';
+import * as passport from "passport";
+import { ApiModule } from './api/api.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RequestsLoggerMiddleware } from './core/middlewares';
@@ -8,11 +13,8 @@ import { ChatsModule } from './modules/chats/chats.module';
 import { HotelsModule } from './modules/hotels/hotels.module';
 import { ReservationsModule } from './modules/reservations/reservations.module';
 import { UsersModule } from './modules/users/users.module';
-import { ApiModule } from './api/api.module';
-import * as cookieParser from 'cookie-parser';
-import * as session from 'express-session';
-import * as passport from "passport";
-import { randomBytes } from 'crypto';
+
+const MongoStore = require("connect-mongo");
 
 @Module({
   imports: [
@@ -42,6 +44,9 @@ export class AppModule {
           secret: process.env.SESSION_SECRET || randomBytes(32).toString('hex'),
           resave: false,
           saveUninitialized: false,
+          store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/hotels',
+          })
         }),
         passport.initialize(),
         passport.session()
